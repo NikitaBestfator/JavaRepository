a004f2e26f2378470413fee552f61a072cc415bb
NikitaBestfator <nbestfator@gmail.com>
2026-08-02 17:23:26 +0300

    закончил тему внутренних классов и очистил старый код в main

---
+++ Proj1/src/Math_2.java
@@ -0,0 +1,24 @@
+public class Math_2 {
+    public static class Factorial{
+        private int result;
+        private int key;
+
+        public Factorial(int number, int x){
+            result=number;
+            key = x;
+        }
+        public int getResult(){
+            return result;
+        }
+        public int getKey(){
+            return key;
+        }
+    }
+    public static Factorial getFactorial(int x){
+        int result = 1;
+        for(int i = 1; i <= x;i++){
+            result *= i;
+        }
+        return new Factorial(result, x);
+    }
+}
