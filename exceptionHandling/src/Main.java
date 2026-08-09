b08a9ea672f2f6c44bd218e693aeaa3a8795abe9
NikitaBestfator <nbestfator@gmail.com>
2026-08-09 14:34:59 +0300

    Закончил главу обработка исключений

--- exceptionHandling/src/Main.java
+++ exceptionHandling/src/Main.java
@@ -1,39 +1,61 @@
-class Program{
-    public static void main(String[] args){
-//        int a = 5;
-//        int b = 0;
-//        int result = a / b;
-//        System.out.printf("Результат: %d\n", result);
-//        divide(10, 2);
-//        divide(10, 0);
-//        System.out.println("Конец программы");
-        int result = parse("5");
-        System.out.println(result);
+import java.util.Scanner;
 
-        result = parse("t");
-        System.out.println(result);
+class Program{
+    public static void main(String[] args) throws Throwable{
+//        createPerson("Tom", -10);
+//        createPerson("m", 20);
+        int f1 = factorial(4);
+        System.out.println("f1: " + f1);
+
+        int f2 = factorial(-4);
+        System.out.println("f2: " + f2);
     }
-//    static void divide(int a, int b){
-//
+    static int factorial(int n){
+        assert n > 0: n;
+
+        int result = 1;
+        while(n > 1) result *= n--;
+        return result;
+    }
+//    static void createPerson(String name, int age){
 //        try{
-//            int result = a / b;
-//            System.out.printf("Результат: %d\n", result);
+//            var person = new Person(name, age);
+//            person.print();
 //        }
-//        catch(ArithmeticException e){
-//            System.out.println("Возникло исключение!");
+//        catch(PersonNameException ex){
+//            System.out.println("Ошибка в имени: ");
+//            System.out.println(ex.getMessage());
 //        }
-//        finally {
-//            System.out.println("Блок finally");
+//        catch(PersonAgeException ex){
+//            System.out.println("Ошибка в возрасте: ");
+//            System.out.println(ex.getMessage());
 //        }
 //    }
-    static int parse(String s){
-        try{
-            return Integer.parseInt(s);
-        }
-        catch(Throwable _){
-            System.out.println("Ошибка преобразования");
-            return 0;
-        }
-    }
 }
-// try...catch...finally
+//class Person{
+//    private String name;
+//    private int age;
+//    private static int nameMinLength = 2;
+//    private static int minAge = 1;
+//    private static int maxAge = 110;
+//
+//    Person(String name, int age) throws PersonNameException, PersonAgeException{
+//        if(name.length() < 2) throw new PersonNameException(name, nameMinLength);
+//        if(age < 1 || age > 110) throw new PersonAgeException(age, minAge, maxAge);
+//        this.name = name;
+//        this.age = age;
+//    }
+//    void print(){System.out.printf("Name: %s; Age: %d\n", name, age);}
+//}
+// Assertion (утверждение) паттерн защитного механизма (guard)
+// во время тестирований проверяет соблюдение некоторых условий
+// assert используется для проверки ошибок при которых нет смысла продолжать программу
+// assert n > 0; - здесь оператор проверяет условие n > 0 если нет сгенерирует ошибку
+// assert n > 0: n - здесь все то же самое,
+// но только значение переменной n передается в конструктор класса AssertionError
+// для пакетов и классов надо включать поддержку assert
+// java -enableassertions Program - включить поддержку
+// java -ea:SomeClass -ea:com.mycompany.somelib... Program включить поддержку для класса SomeClass для пакета
+// com.mycompany.somelib
+// -ea:... - включает в том месте где у пакета нет имени
+// java -ea:... -da: SomeClass Program выключить поддержку assert
